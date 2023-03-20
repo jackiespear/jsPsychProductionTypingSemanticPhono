82432f52dd4695022a20f4e0d4778ad9bd894bc4
“Jackie <56136954+jackiespear@users.noreply.github.com>
2023-03-20 15:11:22 -0500

    Adding Files
    adding experimental files

---
+++ stimuli/study_words_consist_practice.js
@@ -0,0 +1,5 @@
+studywords_practice = [
+{"stimulus":"<p style='font-size: 48px;'>BRICK - <span style='color: red'>TRICK</span></p>","data":{"type":"practice","category":"read","word":"TRICK"}},
+{"stimulus":"<p style='font-size: 48px;'>WRITE - <span style='color: green'>FRIGHT</span></p>","data":{"type":"practice","category":"produce","word":"FRIGHT"}},
+]
+
