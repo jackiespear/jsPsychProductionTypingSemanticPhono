46851756c5417e9532e379f6be939f42f7f9e71a
“Jackie <56136954+jackiespear@users.noreply.github.com>
2023-03-20 20:30:02 -0500

    Adding Firebase info

--- stimuli/study_words_consist_practice.js
+++ stimuli/study_words_consist_practice.js
@@ -1,5 +1,5 @@
 studywords_practice = [
-{"stimulus":"<p style='font-size: 48px;'>BRICK - <span style='color: red'>TRICK</span></p>","data":{"type":"practice","category":"read","word":"TRICK"}},
-{"stimulus":"<p style='font-size: 48px;'>WRITE - <span style='color: green'>FRIGHT</span></p>","data":{"type":"practice","category":"produce","word":"FRIGHT"}},
+{"stimulus":"<p style='font-size: 48px;'><span style='color: red'>TRICK</span></p>","data":{"type":"practice","category":"read","word":"TRICK"}},
+{"stimulus":"<p style='font-size: 48px;'><span style='color: green'>FRIGHT</span></p>","data":{"type":"practice","category":"produce","word":"FRIGHT"}},
 ]
 
